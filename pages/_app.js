26d75c4243623e84f31dae4044ae2cd44cb79c41
Marcus <marcus.gamboa@gmail.com>
2022-02-28 15:19:10 -0700

    start Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import Footer from "../components/layout/Footer.js";
+import Header from "../components/layout/Header.js";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />
       <Footer />
     </>
