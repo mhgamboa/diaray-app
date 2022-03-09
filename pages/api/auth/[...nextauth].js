1a57c15304f1aff519fa4f62d9dc8b96ad606f2e
Marcus <marcus.gamboa@gmail.com>
2022-03-09 15:49:55 -0700

    Add Google OAuth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb.js";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
 // import EmailProvider from "next-auth/providers/email"
 
 // For more information on each option (and a full list of options) go to
@@ -18,5 +21,40 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    // CredentialsProvider({
+    //   name: "email", // `Sign in with ${name}`
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "User Name" },
+    //     password: { label: "Password", type: "password", placeholder: "Password" },
+    //   },
+
+    //   async authorize(credentials, req) {
+    //     // You need to provide your own logic here that takes the credentials
+    //     // submitted and returns either an object representing a user or value
+    //     // that is false/null if the credentials are invalid.
+    //     // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+    //     // You can also use the `req` object to obtain additional parameters
+    //     // (i.e., the request IP address)
+    //     console.log(credentials);
+    //     const res = await fetch("/your/endpoint", {
+    //       method: "POST",
+    //       body: JSON.stringify(credentials),
+    //       headers: { "Content-Type": "application/json" },
+    //     });
+    //     const user = await res.json();
+    //     if (res.ok && user) {
+    //       return user; // If no error and we have user data, return it
+    //     }
+    //     return null; // Return null if user data could not be retrieved
+    //   },
+    // }),
   ],
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24 * 7 * 1, //sec * min * hour * day = 1 week
+  },
 });
